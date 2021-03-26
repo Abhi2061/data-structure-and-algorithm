bba5a24966b050be7e87d876cf4583571faf34b5
Abhi <abhishek2061a@gmail.com>
2021-03-26 22:27:47 +0530

    edited binary search tree

--- BST.cpp
+++ BST.cpp
@@ -276,10 +276,6 @@ int main()
     root = insert(root, 4);
     root = insert(root, 2);
     root = insert(root, 6);
-    root = insert(root, 1);
-    root = insert(root, 3);
-    root = insert(root, 5);
-    root = insert(root, 7);
 
     post_order(root);
     cout << endl;
